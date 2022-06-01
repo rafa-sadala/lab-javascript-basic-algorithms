0bfed68f01eb6f867a5600f5c260d1cd07c5a95f
unknown <rafael.sadala@yahoo.com.br>
2022-06-01 17:03:27 -0300

    int2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,26 @@
 // Iteration 1: Names and Input
-//
+
+const hacker1 = "Rafael";
+const hacker2 = "Google";
+
+console.log("The driver´s name is " + hacker1);
+console.log("The driver´s name is " + hacker2);
+
 // Iteration 2: Conditionals
 
+const a = hacker1.length;
+const b = hacker2.length;
+
+if (a > b) {
+  console.log(`The driver has the longest name, it has ${a} characters.`);
+} else if (a < b) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${a} characters.`
+  );
+} else {
+  console.log(`Wow, you both have equally long names, ${a} characters!`);
+}
 
 // Iteration 3: Loops
+
+console.log(newname);
